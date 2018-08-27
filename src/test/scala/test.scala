8b54e7da9c9b121b95cf00b4115540ceb0159347
daniemc <daniel.meneses27@gmail.com>
2018-08-27 15:45:30 -0500

    add test to string

--- src/test/scala/test.scala
+++ src/test/scala/test.scala
@@ -12,6 +12,12 @@ class test extends FunSuite {
 
   test("type class interface test (interface Syntax)") {
     val res = Person("Daniel Meneses", 21).toJson
+    println(res)
+  }
+
+  test("test implicit stringWriter") {
+    val res = Json.toJson("Hi implicit")
+    println(res)
   }
 
 }
